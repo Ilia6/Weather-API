fb7516711456ef0a79f359befcf4c8e8419f8686
Ilia6 <146102840+Ilia6@users.noreply.github.com>
2024-02-20 18:43:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const apiKey ="fbdf2238581b056532b0674dd6de2fc5";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".humidity").innerHTML = data.wind.speed + "km/h";
+
+
+}
+
+searchBtn.addEventListener("clic", ()=>{
+    checkWeather(searchBox.value);
+})
